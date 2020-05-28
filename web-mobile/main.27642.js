2a8bbac4216240724e416c258977d5e62744e07c
张波 <>
2020-05-28 18:03:29 +0800

    wolf1

--- web-mobile/main.27642.js
+++ web-mobile/main.27642.js
@@ -171,7 +171,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.3d92d.js');
+        require('src/settings.57c5d.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -179,7 +179,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.3d92d.js');
+        require('src/settings.57c5d.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
